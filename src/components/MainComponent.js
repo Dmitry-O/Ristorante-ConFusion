2449d9e21ef6cc06576603a549704fa3d1068a84
Dmitry Olkhovikov <dimasic9420@gmail.com>
2020-11-04 18:11:06 +0200

    Controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -51,7 +51,7 @@ class Main extends Component {
           <Switch>
             <Route path="/home" component={HomePage}/>
             <Route exact path="/contactus" component={Contact}/>
-            <Route exact path="/aboutus" component={About}/>
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId}/>
             <Redirect to="/home" />
