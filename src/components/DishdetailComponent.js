5ae714d56506834eaa1bc2a61ba93e57d86be661
Dmitry Olkhovikov <dimasic9420@gmail.com>
2020-11-08 20:14:47 +0200

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle,
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -37,13 +38,13 @@ function RenderComments({comments, addComment, dishId}) {
 
     function RenderDish({dish}) {
         return (
-        <Card>
-            <CardImg width="100%" src = {dish.image} alt = {dish.name} />
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+            <Card>
+                <CardImg width="100%" src = {baseUrl + dish.image} alt = {dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
         );
     }
 
